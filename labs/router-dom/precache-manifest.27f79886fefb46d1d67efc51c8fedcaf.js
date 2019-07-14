7f19e283729852b361addffb094f8ddccb50cb41
Rodrigoant <rodrigoteajuda@gmail.com>
2019-07-14 00:08:10 -0300

    Add React Router Dom Project

---
+++ labs/router-dom/precache-manifest.27f79886fefb46d1d67efc51c8fedcaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc7f40ee4a32a718cc0308227327407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d21115a83aff0c862fb0",
+    "url": "/static/css/main.b827a028.chunk.css"
+  },
+  {
+    "revision": "b385ca2c3e2da8536937",
+    "url": "/static/js/2.d7a2af6a.chunk.js"
+  },
+  {
+    "revision": "d21115a83aff0c862fb0",
+    "url": "/static/js/main.bd7000bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
